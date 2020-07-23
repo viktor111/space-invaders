a95c6f9581629b769b9e3aed53e282223c3b84aa
viktor111 <swifrorlilko@gmail.com>
2020-07-23 14:32:37 +0300

    add bullet and background

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+import random
 
 pygame.init()
 
 icon = pygame.image.load("spaceship.png")
 player_img = pygame.image.load("player.png")
+enemy_img = pygame.image.load("alien.png")
+background_img = pygame.image.load("background.png")
+bullet_img = pygame.image.load("bullet.png")
 
 WIDTH = 800
 HIEGHT = 600
@@ -14,18 +18,31 @@ display = pygame.display
 display.set_caption("Space Invaders")
 display.set_icon(icon)
 
+enemy_x = random.randint(0, 800) 
+enemy_y = random.randint(45, 150)
+enemy_change_x = 2 
+enemy_change_y = 40
+
+bullet_x = random.randint(0, 800) 
+bullet_y = random.randint(45, 150)
+bullet_change_x = 2 
+bullet_change_y = 40
+
 player_x = 370
 player_y = 480
-
 player_change_x = 0
 
 def player(x, y):
     screen.blit(player_img, (x, y))
 
+def enemy(x, y):
+    screen.blit(enemy_img, (x, y))
+
 running = True
 while running:
 
     screen.fill((0,0,0))
+    screen.blit(background_img, (0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,17 +50,32 @@ while running:
             
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_change_x = -2.7
+                player_change_x = -3.5
                 print("<")
 
             if event.key == pygame.K_RIGHT:
-                player_change_x = 2.7
+                player_change_x = 3.5
                 print(">")
 
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-               print("release key") 
-
+                print("release key") 
+                player_change_x = 0
+                
     player_x += player_change_x
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 736:
+        player_x = 736
+
+    enemy_x += enemy_change_x
+    if enemy_x <= 0:
+        enemy_y += enemy_change_y
+        enemy_change_x = 2
+    elif enemy_x >= 736:
+        enemy_y += enemy_change_y
+        enemy_change_x = -2
+        
     player(player_x, player_y)
+    enemy(enemy_x, enemy_y)
     display.update()
